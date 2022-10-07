fc775aa32b2ec848ce579021da41e43d4dc5d403
Villemson <115221468+Villemson@users.noreply.github.com>
2022-10-07 14:08:00 +0300

    Add files via upload
    Programm 2

---
+++ Prog2.py
@@ -0,0 +1,19 @@
+arv1=int(input("Esimene arv "))
+arv2=int(input("Teine arv "))
+# soovitus   if (arv1>arv2):
+if arv1>arv2:
+    print("Esimene arv on suurem")
+else:
+    if(arv2>arv1):
+        print("Teine arv on suurem")
+    else:
+        print("Arvud on võrdsed")
+if(arv1>=arv2):
+    maksimum=arv1
+else:
+    maksimum=arv2
+    print("maksimum on = ",maksimum )
+
+
+        
+
